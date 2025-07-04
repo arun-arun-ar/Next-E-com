c34bead26c841a9c03809e05ba221503d140a138
arun-arun-ar <arunacharya2056@gmail.com>
2025-07-04 21:30:53 +0545

    backup code

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<>
+<div  className='w-full'>
+    <div className='max-w-7xl sm:px-4 md:px-4 lg:px-0'>
+
+    </div>
+</div>
+</>
+)
+}
+
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
     <div>
       <h1>Welcome</h1>
+      <Footer />
     </div>
     </>
   );
